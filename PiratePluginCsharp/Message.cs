1fd2efa108d0ecca13ca15516000f21839d4f7ec
Tim Heuer <tim@timheuer.com>
2024-01-18 18:44:59 -0800

    Add project files.

---
+++ PiratePluginCsharp/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace PiratePluginCsharp;
+
+public class Message
+{
+    [System.Text.Json.Serialization.JsonPropertyName("role")]
+    public string Role { get; set; }
+    [System.Text.Json.Serialization.JsonPropertyName("content")]
+    public string Content { get; set; }
+    [System.Text.Json.Serialization.JsonPropertyName("copilot_references")]
+    public object[] References { get; set; }
+}
+
+public class Data
+{
+    [System.Text.Json.Serialization.JsonPropertyName("messages")]
+    public Message[] messages { get; set; }
+}
---
+++ PiratePluginCsharp/Program.cs
@@ -0,0 +1,81 @@
+using Azure;
+using Azure.AI.OpenAI;
+using PiratePluginCsharp;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using Choice = PiratePluginCsharp.Choice;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+#region throwaway callback method
+app.Map("/oauth/callback", () =>
+{
+    Debug.WriteLine("OAuth callback");
+    return Results.Ok();
+})
+.WithName("oauthcallback")
+.WithOpenApi();
+#endregion
+
+app.MapPost("/arrrr", async (Data data) =>
+{
+    Uri azureOpenAIResourceUri = new(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_ENDPOINT"));
+    AzureKeyCredential azureOpenAIApiKey = new(Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY"));
+    OpenAIClient client = new(azureOpenAIResourceUri, azureOpenAIApiKey);
+
+    var chatCompletionsOptions = new ChatCompletionsOptions()
+    {
+        DeploymentName = "chat", // Use DeploymentName for "model" with non-Azure clients
+        Messages =
+            {
+                // The system message represents instructions or other guidance about how the assistant should behave
+                new ChatRequestSystemMessage("You are a helpful assistant. You will talk like a pirate. You will limite your response to only two sentences each time."),
+                // User messages represent current or historical input from the end user
+                new ChatRequestUserMessage("Can you help me?"),
+                // Assistant messages represent historical responses from the assistant
+                new ChatRequestAssistantMessage("Arrrr! Of course, me hearty! What can I do for ye?"),
+                new ChatRequestUserMessage("What's the best way to train a parrot?"),
+            }
+    };
+
+    // this feels super hack, still doesn't work even
+    // feels like i'm using the Azure SDK wrong and that I should be more easily be able to return the streamed responses
+    async IAsyncEnumerable<StreamingChatCompletionsUpdate> StreamContentUpdates()
+    {
+        var responseStream = await client.GetChatCompletionsStreamingAsync(chatCompletionsOptions);
+        await foreach (var response in responseStream)
+        {
+            if (!string.IsNullOrEmpty(response.ContentUpdate))
+            {
+                yield return response;
+            }
+        }
+    }
+    
+    return StreamContentUpdates();
+    
+    
+    // stuck here
+})
+.WithName("arrrr")
+.WithOpenApi();
+
+app.UseHttpsRedirection();
+
+app.Run();
